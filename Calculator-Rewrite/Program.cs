3bbcac386a7a6a54282dbfe29beccbb081b85288
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2022-04-23 22:20:01 +0100

    added the 'operation' variable
    created the operation variable to store the user's desired operation,
    this will later be used to add the if statements or switch cases.
    
    also added a message asking the user for their input.

--- Calculator-Rewrite/Program.cs
+++ Calculator-Rewrite/Program.cs
@@ -6,7 +6,8 @@ namespace Calculator_rewrite
     {
         public static void Main(string[] args)
         {
-
+            Console.WriteLine("Please select one of the following operations\nAddition\nSubtraction\nMultiplication\nDivision");
+            string operation = Console.ReadLine().ToLower();
         }
     }
 }
