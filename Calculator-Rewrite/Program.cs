6ed8a34ecf910f9f9786941c496ba8f01c0d27df
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2022-04-23 22:23:26 +0100

    declared variables
    declared a variable to hold the first number.
    declared a variable to hold the second number.
    declared a variable to hold the user's input.
    
    these variables will be used in the switch case.

--- Calculator-Rewrite/Program.cs
+++ Calculator-Rewrite/Program.cs
@@ -9,6 +9,11 @@ namespace Calculator_rewrite
             Console.WriteLine("Please select one of the following operations\nAddition\nSubtraction\nMultiplication\nDivision");
             string operation = Console.ReadLine().ToLower();
 
+            int firstnumber;
+            int secondNumber;
+
+            string userInput;
+
             switch (operation)
             {
                 case "addition":
